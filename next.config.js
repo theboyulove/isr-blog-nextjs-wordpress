d154031cbb6b1b2a688274d28dd52cd2353539a0
theboyulove <124706303+theboyulove@users.noreply.github.com>
2023-05-08 21:54:51 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,20 +1,4 @@
-if (!process.env.WORDPRESS_API_URL) {
-  throw new Error(`
-    Please provide a valid WordPress instance URL.
-    Add to your environment variables WORDPRESS_API_URL.
-  `)
-}
-
 module.exports = {
-  images: {
-    domains: [
-      process.env.WORDPRESS_API_URL.match(/(?!(w+)\.)\w*(?:\w+\.)+\w+/)[0], // Valid WP Image domain.
-      '0.gravatar.com',
-      '1.gravatar.com',
-      '2.gravatar.com',
-      'secure.gravatar.com',
-    ],
-  },
   async redirects() {
     return [
       {
@@ -40,5 +24,14 @@ module.exports = {
         permanent: false,
       },
     ]
-  }
+  },
+  images: {
+    domains: [
+      process.env.WORDPRESS_API_URL.match(/(?!(w+)\.)\w*(?:\w+\.)+\w+/)[0], // Valid WP Image domain.
+      '0.gravatar.com',
+      '1.gravatar.com',
+      '2.gravatar.com',
+      'secure.gravatar.com',
+    ],
+  },
 }
